2b1091ffa9e7661d7918e9d8daff6669db3a97c3
jenish2222 <102413064+jenish2222@users.noreply.github.com>
2022-07-12 12:22:14 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setuptools.setup(
     url="https://github.com/PrithivirajDamodaran/Gramformer.git",
     packages=setuptools.find_packages(),
     #install_requires=['transformers', 'sentencepiece==0.1.95', 'python-Levenshtein==0.12.2', 'fuzzywuzzy==0.18.0',  'tokenizers==0.10.2', 'fsspec==2021.5.0', 'lm-scorer==0.4.2', 'errant'],
-    install_requires=['transformers', 'sentencepiece', 'python-Levenshtein', 'fuzzywuzzy',  'tokenizers', 'fsspec', 'errant'],
+    install_requires=['transformers', 'sentencepiece', 'python-Levenshtein', 'fuzzywuzzy',  'tokenizers', 'fsspec', 'git+https://github.com/jenish2222/errant.git'],
     classifiers=[
         "Programming Language :: Python :: 3.7",
         "License :: Apache 2.0",
